a4ee0eaf98a714d45b60498f04fba1c6adb620dc
wong <wangtx@wsights.com>
2021-07-19 14:29:05 +0800

    测试  demo

---
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wongtx
+ * @version 1.0
+ * @date 2021/7/19 14:25
+ */
+@RestController
+public class DemoController {
+    private void ce()  {
+        System.err.println("ces");
+}
+}
